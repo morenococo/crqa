cc24f8e4d2bb88e06279c6fa75bb24243cbd11e5
moreno <moreno.cocoi@gmail.com>
2018-10-12 15:50:53 +0100

    there was a typo in crqa_1.0.7 whereby a constant value was left assigned to the radius.

--- R/drpdfromts.R
+++ R/drpdfromts.R
@@ -5,82 +5,63 @@
 # categorical time series, t1 t2 (x / y)
 # the ws is the total nr. of lags over which the series are evaluated
 
+# modified as of crqa.1.0.7, now we use the crqa function and directly
+# extract from it the diagonal means. This change was implemented to obtain 
+# diagonal profiles from delayed and embedded continuous time-series
+
+# the drpdfromts assumes as default categorical variables, 
+# no-delay, no-embed, and radius < 0.001
+
 .packageName <- 'crqa'
 
-drpdfromts <- function(t1, t2, ws, datatype, radius){
-
-drpd = vector();
-
-if (datatype == "categorical"){ ## just to make sure that data is given as vector
-
-    t1 = as.character( as.matrix( t1 ) )
-    t2 = as.character( as.matrix( t2 ) )
-
-}
-
-if (datatype == "continuous"){
-    
-    t1 = as.numeric( as.matrix( t1 ) )
-    t2 = as.numeric( as.matrix( t2 ) )
-
-}
-
-
-for (i in (-ws-1):-2){
+drpdfromts <- function(ts1, ts2, ws, datatype, 
+                       radius = 0.001, delay = 1, embed = 1, rescale = 0,
+                       normalize = 0, mindiagline = 2, minvertline = 2,
+                       tw = 0){
   
-    ix = abs(i);
-    y = t2[ix:length(t2)];
-    x = t1[1:length(y)];
-
-    if (datatype == "categorical"){
-
-        drpd = c(drpd, sum(y==x)/length(y))}
+  if (datatype == "categorical"){ 
+    ## convert the data into numeric
+    ts1     = as.character( as.matrix( ts1 ) )
+    ts2     = as.character( as.matrix( ts2 ) )
     
-    if (datatype == "continuous"){
-        
-        dif = abs(x-y)
-        recpoint = length( which(dif <= radius) )
-        drpd = c(drpd, recpoint / length(y))
-
-    }
+    ## apply convenient function to transform the levels of the categorical variables into numerics
+    checked = checkts(ts1, ts2, datatype, thrshd = length(ts1), pad = FALSE)
+    ts1     = checked[[1]][, 1]
+    ts2     = checked[[1]][, 2]
     
-    ## drpd = [drpd ; sum(y==x)/sum((y<9|x<9))]; if we have to conditionalize the probabilities
-}
-
-if (datatype == "categorical"){
-    drpd = c(drpd,  sum(t1==t2)/length(t1)); ## this is lag 0    
-}
-
-if (datatype == "continuous"){
-    dif = abs(t1-t2)
-    recpoint = length( which(dif <= radius) )
-    drpd = c(drpd, recpoint / length(t1))
-}
-
-
-
-for (i in 2:(ws+1)){
+  }
+  
+  if (datatype == "continuous"){
+    ## just double check that the data is numeric
+    ts1 = as.numeric( as.matrix( ts1 ) )
+    ts2 = as.numeric( as.matrix( ts2 ) )
     
-    x = t1[i:length(t1)]
-    y = t2[1:length(x)]
+  }
+  
+  ## run full blown crqa
+  res = crqa(ts2, ts1, delay, embed, rescale, radius, 
+             normalize, mindiagline, minvertline, tw)
+
+  ## extract the RP
+  RP = res$RP
+  if (class(RP) != "logical"){ # we have some point that recur
+    len      = nrow(RP)-1 # the nrow of the RP
+    lags     = -len:len   # the diagonal of the RP
     
-    if (datatype == "categorical"){
-        drpd = c(drpd,  sum(y==x)/length(y)) ## this is lag 0    
-    }
+    RP      = matrix(as.numeric(RP), nrow = nrow(RP), ncol = ncol(RP))
+    RP_form = unlist(lapply(split(RP, row(RP) - col(RP)), mean, na.rm = TRUE))
+    wn      = which(lags >= -ws &  lags <= ws)
     
-    if (datatype == "continuous"){
-        dif = abs(x-y)
-        recpoint = length( which(dif <= radius) )
-        drpd = c(drpd, recpoint / length(y))
-    }
-
-}
-
-## extract  max recurrence and the lag at which it occurred
-
-maxrec = max(drpd);
-maxlag = which(drpd == maxrec);
-
-return( list(profile = drpd, maxrec = maxrec, maxlag = maxlag) )
-
+    drpd    = RP_form[wn]
+  } else {
+    drpd    = 0 # or technically it should be an NA
+  }
+  
+  ## extract  max recurrence and the lag at which it occurred
+  
+  maxrec = max(drpd);
+  maxlag = which(drpd == maxrec);
+  
+  return( list(profile = drpd, maxrec = maxrec, maxlag = maxlag) )
+  
 }
