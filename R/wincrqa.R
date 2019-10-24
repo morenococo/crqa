275f7cf9de68691bc984adfecbbed476ddce6e36
Moreno Coco <mcoco@uel.ac.uk>
2019-10-24 17:37:52 +0100

    updated to the new core crqa() function

--- R/wincrqa.R
+++ R/wincrqa.R
@@ -23,37 +23,35 @@
 .packageName <- 'crqa'
 
 wincrqa <- function(ts1, ts2, windowstep, windowsize, delay, embed,
-                    rescale, radius, normalize, mindiagline,
-                    minvertline, tw = 0, whiteline = F, trend = FALSE){
+                    radius = 0.001, rescale = 0,
+                    normalize = 0, mindiagline = 2, minvertline = 2,
+                    tw = 0, whiteline = FALSE, recpt = FALSE, side = 'both', 
+                    method = 'crqa', metric = 'euclidean', 
+                    datatype = 'continuous', trend = FALSE){
 
     ## we do not expect as input a recurrent plot
     ts1 = as.vector(as.matrix(ts1));   ts2 = as.vector(as.matrix(ts2))
     points = seq(1, (length(ts1) - (windowsize)-1), windowstep)
     
-    TREND = crawin = profile = vector()
+    TREND = crawin = vector()
     tsp = 0 ## set a counter with the win at which rec was found    
 
     i = 1
     for (i in points){
         tsp = tsp +1
         
-        ts1win = ts1[i:(i+windowsize - 1)];
-        ts2win = ts2[i:(i+windowsize - 1)];
+        ts1win = ts1[i:(i + windowsize - 1)];
+        ts2win = ts2[i:(i + windowsize - 1)];
         
         ans = crqa(ts1win, ts2win, delay, embed, rescale,
-            radius, normalize, mindiagline, minvertline, tw,
-            whiteline)
+                   radius, normalize, mindiagline, minvertline,
+                   tw, whiteline, recpt, side, method, metric,
+                   datatype)
         
         RP = ans$RP
-        
-        if (length(RP) == 1){ 
-          RP = vector() ## a trick for cases with empty recurrence plot
-        } else {
-          prof  = fulldiagprof(matrix(RP, ncol = ncol(RP), nrow = nrow(RP)))
-          profile = c(profile, mean(prof[,2], na.rm = TRUE))          
-        }                                 
-        
-        ans = as.numeric( unlist(ans[1:9]) )
+        if (length(RP) == 1) RP = vector() ## a trick for cases
+                                           ## with empty recurrence plot
+        ans = as.numeric( unlist(ans[1:10]) )
         ## if trend needs to be calculated do it here
 
         if (trend == TRUE){
@@ -81,11 +79,16 @@ wincrqa <- function(ts1, ts2, windowstep, windowsize, delay, embed,
             TREND = c(TREND, NA)
         }
         
+        
         crawin = rbind(crawin, c(ans, tsp), deparse.level = 0)
         
     }
  
-    return(list(crqwin = crawin, TREND = TREND, profile = profile))
+    ## name the measures
+    colnames(crawin) = c("RR", "DET", "NRLINE", "maxL", "L", 
+                         "ENTR", "rENTR", "LAM", "TT", "catH", "TREND")
+    
+    return(list(crqwin = crawin, TREND = TREND))
     
 }
 
