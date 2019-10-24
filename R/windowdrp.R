be793d45163e4e10a898cbb9e4b0a90e637fcfc3
Moreno Coco <mcoco@uel.ac.uk>
2019-10-24 17:42:45 +0100

    updated to work with the new crqa() core function

--- R/windowdrp.R
+++ R/windowdrp.R
@@ -2,35 +2,43 @@
 ## original Matlab code by Rick Dale
 
 ## calculate recurrence over different sized windows
-## arguments: step = interval considered on the serie;
+## arguments: windowstep = interval considered on the serie;
 ##            window_size = the size of the window wherin crqa is runned. 
 ##            lag_profile_width = lags within the window
 
-# step = 10;
-# window_size = 100;
-# lag_profile_width = 20;
-
-# source("drpd_fromts.R")
+## windowstep = 10;
+## windowsize = 100;
+## lagwidth = 20;
+## datatype = "categorical"
 
 .packageName <- 'crqa'
 
-windowdrp <- function(x, y, step, windowsize, lagwidth, datatype, radius){
+windowdrp <- function(ts1, ts2, windowstep, windowsize, lagwidth, 
+                      radius = 0.001, delay = 1, embed = 1, rescale = 0,
+                      normalize = 0, mindiagline = 2, minvertline = 2,
+                      tw = 0, whiteline = FALSE, recpt = FALSE, side = 'both', 
+                      method = 'crqa', metric = 'euclidean', 
+                      datatype = 'continuous'){
   
-x = as.vector(as.matrix(x));   y = as.vector(as.matrix(y));
-points = seq(1, (length(x) - (windowsize)-1), step)
+ts1 = as.vector(as.matrix(ts1));   ts2 = as.vector(as.matrix(ts2));
+dpoint = seq(1, (length(ts1) - (windowsize)-1), windowstep)
 
 drpd = vector() ## a vector to store all the points
-
-for (i in points){
+i = 1
+for (i in dpoint){
   
-  xwin = x[i:(i+windowsize)];
-  ywin = y[i:(i+windowsize)];
+  ts1win = ts1[i:(i+windowsize - 1)];
+  ts2win = ts2[i:(i+windowsize - 1)];
   
   drpd = c(drpd,
-      mean(drpdfromts(xwin, ywin, lagwidth, datatype, radius)$profile))
-      ## can also take the max here
-
-}
+      mean(drpdfromts(ts1win, ts2win,lagwidth,
+                      radius, delay, embed, rescale,
+                      normalize, mindiagline, minvertline,
+                      tw, whiteline, recpt, side, 
+                      method, metric, 
+                      datatype)$profile))
+  
+  }
 
 maxrec = max(drpd);
 maxlag = which(drpd == maxrec);
@@ -38,5 +46,3 @@ maxlag = which(drpd == maxrec);
 return( list(profile = drpd, maxrec = maxrec, maxlag = maxlag) )
 
 }
-
-
