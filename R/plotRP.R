5073ef4d12e579099b7d8c938ba8e115e86b9f97
Alexandra Paxton <paxton.alexandra@gmail.com>
2020-04-08 14:46:38 -0400

    Roll back commits so modifications are on a new branch

--- R/plotRP.R
+++ R/plotRP.R
@@ -1,54 +1,37 @@
-## plotRP(): Function to plot the RP
-##
-## Arguments:
-##     RP        : the RP ngCMatrix output from crqa() function
-##     labelx    : character ; the text label of the x-axis 
-##                 (default: "Time")
-##     labely    : character ; the text label of the y-axis 
-##                 (default: "Time")
-##     labelmain : character ; main title text of the plot 
-##                 (default: "Recurrence Plot")
-##     point_col : character ; the color for the recurrent points;
-##                 may include any colors from the base R plot repertoire
-##                 (default: "black)
-##     pcex      : numeric ; the size of the recurrent points
-##                 (default: .3)
-##     pch       : numeric ; the style of the recurrent points
-##                 (default: 1)
-##     show_ticks: boolean ; whether to show x- and y-ticks or not
-##                 (default: FALSE)
-##     unit      : numeric ; gap between sample labeling on axes.
-##                 Note: only relevant if `show_ticks = TRUE`.
-##                 (default: 10)
-##
-## Value:
-##     A square plot visualizing the recurrence matrix.
-##
-## Author(s): Moreno I. Coco & Alexandra Paxton
+## plot the RP
+## need to be improved, need to take out the for() loop for it
+## RP  : the RP plot
+## par : a list of parameters for the plotting
+##     unit:   the unit to mark ticks on the axes
+##     labelx: the label of the x-axis  
+##     labely: the label of the y-axis  
+##     cols  : the color for the recurrent recurrent points
+##     pcex   : the size of the dots
 
 .packageName <- 'crqa'
 
-plotRP <- function(RP, 
-                   labelx = "Time", labely = "Time", 
-                   labelmain = "Recurrence Plot",
-                   point_col = "black", pcex = .3, pch = 1,
-                   unit = 10, show_ticks = FALSE){
+plotRP <- function(RP, par){
   
-  # find the size of the RP
+  # if no user-defined parameters exist, use defaults
+  if (exists("par") == FALSE){
+    labelx = "Time"; labely = "Time"; labelmain = "Recurrence Plot";
+    unit  = 2
+    cols  = "black"; pcex = .3; pch = 1; las = 0;
+    labax = seq(0, nrow(RP), unit); labay = seq(0, nrow(RP), unit);
+  } else { # we load the values that we desire
+    for (v in 1:length(par)) assign(names(par)[v], par[[v]])
+  }
+  
   xdim   = nrow(RP)
   ydim   = ncol(RP)
   
-  # transform the RP into a square matrix
-  RP = matrix(as.numeric(RP), nrow = xdim, ncol = ydim)
+  RP = matrix(as.numeric(RP), nrow = xdim, ncol = ydim) # transform it for plotting
   
-  # figure out where the recurrent points are
   ind = which(RP == 1, arr.ind = T)
   
-  # create all time series of all possible samples on axis
-  tstamp = seq(0, xdim, 1)
+  tstamp = seq(0, xdim, unit)
   
-  # create the shell of the plot
-  par(mar = c(3.8, 3.8, 2,2), font.axis = 2, cex.axis = 1,
+  par(mar = c(3.8, 3.8, 0.2,2), font.axis = 2, cex.axis = 1,
       font.lab = 2, cex.lab = 1.2)
   plot(tstamp, tstamp, type = "n", 
        xlab = "", ylab = "", main=labelmain,
@@ -56,24 +39,19 @@ plotRP <- function(RP,
        font = 2)
   
   # add recurrent points to the plot
-  matpoints(ind[,1], ind[,2],  cex = pcex, col = point_col, pch = pch) 
+  matpoints(ind[,1], ind[,2],  cex = pcex, col = cols, pch = pch) 
   
-  # add x- and y-axis labels to the plot (so they're not too far away)
-  mtext(labelx, at = mean(tstamp), side = 1, line = 1.1, cex = 1.2, font = 2)
-  mtext(labely, at = mean(tstamp), side = 2, line = 1.1, cex = 1.2, font = 2)
+  mtext(labelx, at = mean(tstamp), side = 1, line = 2.2, cex = 1.2, font = 2)
+  mtext(labely, at = mean(tstamp), side = 2, line = 2.2, cex = 1.2, font = 2)
   
-  # if the user would like x- and y-tickmarks, show and label them
-  if (show_ticks == TRUE){
-    labax = seq(0, nrow(RP), unit)
-    labay = seq(0, nrow(RP), unit)
-    las = 0
-    mtext(labax, at = labax, side = 1, line = .1, cex = .8, font = 2, las = las)
-    mtext(labay, at = labay, side = 2, line = .1, cex = .8, font = 2, las = las)
-  }
   
-  # create variable to save plot
-  output_plot = recordPlot()
+  #  if (is.numeric(labax)){ ## it means there is some default
+  #    mtext(labax, at = seq(1, nrow(RP), nrow(RP)/10), side = 1, line = .5, cex = 1, font = 2)
+  #    mtext(labay, at = seq(1, nrow(RP), nrow(RP)/10), side = 2, line = .5, cex = 1, font = 2)
+  #  } else{
+  mtext(labax, at = tstamp, side = 1, line = .5, cex = .8, font = 2, las = las)
+  mtext(labay, at = tstamp, side = 2, line = .5, cex = .8, font = 2, las = las)
   
-  # return the plot
-  return(output_plot)
+  # }
+  
 }
