331bd0fa3fe8f1bcc7b33fa99dfb15c14269db28
morenococo <moreno.cocoi@gmail.com>
2020-10-29 11:08:17 +0000

    updated windowdrp() to work with multidimensional time-series. Adjusted indexing of window to handle irregularly size/step.

--- R/windowdrp.R
+++ R/windowdrp.R
@@ -20,29 +20,72 @@ windowdrp <- function(ts1, ts2, windowstep, windowsize, lagwidth,
                       method = 'crqa', metric = 'euclidean', 
                       datatype = 'continuous'){
   
-ts1 = as.vector(as.matrix(ts1));   ts2 = as.vector(as.matrix(ts2));
-dpoint = seq(1, (length(ts1) - (windowsize)-1), windowstep)
-
-drpd = vector() ## a vector to store all the points
-i = 1
-for (i in dpoint){
+  irregular = FALSE; # initialize a flag that will be used to check whether the last window is included
+  ## check the different contexts in which the analyses are running
   
-  ts1win = ts1[i:(i+windowsize - 1)];
-  ts2win = ts2[i:(i+windowsize - 1)];
-  
-  drpd = c(drpd,
-      mean(drpfromts(ts1win, ts2win,lagwidth,
-                      radius, delay, embed, rescale,
-                      normalize, mindiagline, minvertline,
-                      tw, whiteline, recpt, side, 
-                      method, metric, 
-                      datatype)$profile))
-  
+  if (method == "rqa"){
+    ## check that the user as provided the same data to allow windowed recurrence
+    if (exists("ts2")) ts2 = ts2 else stop("Please provide the same vector as argument of t2")
   }
-
-maxrec = max(drpd);
-maxlag = which(drpd == maxrec);
-
-return( list(profile = drpd, maxrec = maxrec, maxlag = maxlag) )
-
+  
+  if (method == "crqa"){
+    ts1 = as.vector(as.matrix(ts1));   ts2 = as.vector(as.matrix(ts2))
+    maxd = length(ts1) ## the total number of points
+    points = seq(1, (maxd - (windowsize)-1), windowstep)
+  }
+  
+  if (method == "mdcrqa"){
+    ts1 = as.matrix(ts1);   ts2 = as.matrix(ts2)
+    maxd = nrow(ts1) ## the total number of points
+    points = seq(1, (maxd - (windowsize)-1), windowstep)
+  }
+  
+  ## need to make sure to include also "irregular" final segments
+  fpoint = points[length(points)] ## beginning of last window
+  ## as the dimension of the data often may not be that precise
+  if (fpoint != (fpoint + windowsize)){
+    irregular = TRUE ## flag used later on
+    fpoint = fpoint + windowsize # update last point
+    points = c(points,  fpoint) ## add final point
+  }
+  
+  
+  # i = 1
+  drpd = vector() ## a vector to store all the points
+  
+  for (i in points){
+    if (irregular == TRUE & i == fpoint){ ## this is the last window to consider 
+      ixs = i:maxd 
+      # warning(paste("Your latest window was shorter: ", length(ixs)))
+    } else {
+      ixs = i:(i + windowsize - 1)
+    }
+    
+    if (method == "crqa" | method == "rqa"){
+      ## the data consists of two vectors  
+      ts1win = ts1[ixs];
+      ts2win = ts2[ixs];
+    }
+    
+    if (method == "mdcrqa"){
+      ## the data consists of two matrices 
+      ts1win = ts1[ixs, ];
+      ts2win = ts2[ixs, ];
+    }
+    
+    drpd = c(drpd,
+             mean(drpfromts(ts1win, ts2win,lagwidth,
+                            radius, delay, embed, rescale,
+                            normalize, mindiagline, minvertline,
+                            tw, whiteline, recpt, side, 
+                            method, metric, 
+                            datatype)$profile))
+    
+  }
+  
+  maxrec = max(drpd);
+  maxlag = which(drpd == maxrec);
+  
+  return( list(profile = drpd, maxrec = maxrec, maxlag = maxlag) )
+  
 }
