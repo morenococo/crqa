7fc7c2a95e258fcaab7f9bca9fa8e7f9157ae466
Alexandra Paxton <paxton.alexandra@gmail.com>
2023-11-03 17:11:12 -0400

    Allow windowed recurrence to be performed on a provided recurrence matrix

--- R/wincrqa.R
+++ R/wincrqa.R
@@ -76,57 +76,85 @@ wincrqa <- function(ts1,
                     metric = 'euclidean', 
                     datatype = 'continuous', 
                     trend = FALSE){
+  
+  ## stop immediately if the windowsize is smaller than delay*phase AND we're not supplying an RP
+  if ((windowsize < embed*delay) & recpt == FALSE){ 
+    stop("Phase-space (embed*delay) longer than windowsize")
+  }  
+  
+  # initialize a flag that will be used to check whether the last window is included
+  irregular = FALSE
+  
+  ## check the different contexts in which the analyses are running
+  if (recpt == FALSE){
     
-    ## stop immediately if the windowsize is smaller than delay*phase
-    if (windowsize < embed*delay){ stop("Phase-space (embed*delay) longer than windowsize")}  
-    
-    irregular = FALSE; # initialize a flag that will be used to check whether the last window is included
-    
-    ## check the different contexts in which the analyses are running
+    # for RQA: check that the user as provided the same data to allow windowed recurrence
     if (method == "rqa"){
-      ## check that the user as provided the same data to allow windowed recurrence
-      if (exists("ts2")) ts2 = ts2 else stop("Please provide the same vector as argument of t2")
-      maxd = length(ts1) ## the total number of points
-      points = seq(1, (maxd - (windowsize)-1), windowstep)    
-    }
-    
-    if (method == "crqa"){
-      ts1 = as.vector(as.matrix(ts1));   ts2 = as.vector(as.matrix(ts2))
-      maxd = length(ts1) ## the total number of points
-      points = seq(1, (maxd - (windowsize)-1), windowstep)
-    }
-    
-    if (method == "mdcrqa"){
-      ts1 = as.matrix(ts1);   ts2 = as.matrix(ts2)
-      maxd = nrow(ts1) ## the total number of points
-      points = seq(1, (maxd - (windowsize)-1), windowstep)
-    }
-    
-    
-    ## need to make sure to include also "irregular" final segments
-    fpoint = points[length(points)] ## beginning of last window
-    ## as the dimension of the data may often be not that precise
-    if (fpoint != (fpoint + windowsize)){
-      irregular = TRUE ## flag used later on
-      fpoint = fpoint + windowsize # update last point
-      points = c(points,  fpoint) ## add final point
-    }
-    
-    
-    crqwin = vector() ## initialize the matrix to be filled with crq results
-    tsp = 0 ## set a counter with all windows at which rec was computed    
-    
-    i = 1
-    for (i in points){
-      tsp = tsp +1
-      
-      if (irregular == TRUE & i == fpoint){ ## this is the last window to consider 
-        ixs = i:maxd 
-        # warning(paste("Your latest window was shorter: ", length(ixs)))
+      if (exists("ts2")) {
+        ts2 = ts2 
       } else {
-        ixs = i:(i + windowsize - 1)
+        stop("Please provide the same vector as argument of t2")
       }
+      maxd = length(ts1) ## the total number of points
+    }
+    
+    # for CRQA
+    if (method == "crqa"){
+      ts1 = as.vector(as.matrix(ts1))
+      ts2 = as.vector(as.matrix(ts2))
+      maxd = length(ts1) ## the total number of points
       
+    }
+    
+    # for MdCRQA
+    if (method == "mdcrqa"){
+      ts1 = as.matrix(ts1)
+      ts2 = as.matrix(ts2)
+      maxd = nrow(ts1) # the total number of points
+    }
+  } else {
+    # for calculating from a supplied matrix
+    logical_RP = as.logical(ts1)
+    v1l = nrow(ts1)
+    ts1 = matrix(logical_RP, 
+                 ncol = as.numeric(v1l))
+    v1l = nrow(ts1)
+    v2l = ncol(ts1)
+    ind = which(ts1 > 0, arr.ind = TRUE)
+    r = ind[,1]
+    c = ind[,2]
+    
+    # total possible number of points on the RP
+    maxd = v1l
+  }
+  
+  ## need to make sure to include also "irregular" final segments
+  points = seq(1, (maxd - (windowsize)-1), windowstep)
+  fpoint = points[length(points)] ## beginning of last window
+  
+  ## as the dimension of the data may often be not that precise
+  if (fpoint != (fpoint + windowsize)){
+    irregular = TRUE ## flag used later on
+    fpoint = fpoint + windowsize # update last point
+    points = c(points,  fpoint) ## add final point
+  }
+  
+  
+  crqwin = vector() ## initialize the matrix to be filled with crq results
+  tsp = 0 ## set a counter with all windows at which rec was computed    
+  
+  i = 1
+  for (i in points){
+    tsp = tsp +1
+    
+    if (irregular == TRUE & i == fpoint){ ## this is the last window to consider 
+      ixs = i:maxd 
+      # warning(paste("Your latest window was shorter: ", length(ixs)))
+    } else {
+      ixs = i:(i + windowsize - 1)
+    }
+    
+    if (recpt == FALSE){
       if (method == "crqa" | method == "rqa"){
         ## the data consists of two vectors  
         ts1win = ts1[ixs];
@@ -138,62 +166,69 @@ wincrqa <- function(ts1,
         ts1win = ts1[ixs, ];
         ts2win = ts2[ixs, ];
       }
-      
-      ## check whether the windowsize can accomodate the delay*embed
-      if (length(ts1win) > delay*embed){
-        
-        ans = crqa(ts1win, ts2win, delay, embed, rescale,
-                   radius, normalize, mindiagline, minvertline,
-                   tw, whiteline, recpt, side, method, metric,
-                   datatype)
-        
-        RP = ans$RP
-        if (length(RP) == 1) RP = vector() ## a trick for cases
-        ## with empty recurrence plot
-        ans = as.numeric( unlist(ans[1:10]) )
-        ## if trend needs to be calculated do it here
-        
-        if (trend == TRUE){
-          
-          if (length(RP) > 0){
-            RP = as.matrix(RP) ## diags() has changed behaviour 
-            NX = ncol(RP)
-            T = vector("numeric", length = NX-1)
-            
-            ## below some test matrices
-            # RP = matrix(1:16,nrow=4)
-            # RP = cbind(c(2,7), c(1,9), c(10, 12))
-            
-            # do we need to span the full matrix or just half?
-            ## create a vector of indeces for all diagonals of the matrix  
-            # ixi_diag = -(ncol(RP)-1):(nrow(RP)-1) 
-            
-            # k = 2
-            for (k in 1:(NX-1)){  ##ixi_diag
-              # print(diags_extract(RP, k)) ## check the diagonal
-              ixi_rec = which(diags_extract(RP, k) != F) ## find recurrence
-              T[k] = length(ixi_rec)/ (NX-k)*100;
-            }
-            
-            Ntau = NX - 1 - round(0.1*NX);
-            
-            ## last 10% of the RP will be skipped
-            p = polyfit(2:(Ntau+1),T[1:Ntau], 1) # slope
-            TREND = 1000 * p[1]
-            ## Webber's definition includes factor 1000
-            
-          } else { TREND = NA}
-        } else {
-          TREND = NA
-        }
-        
-        crqwin = rbind(crqwin, c(ans, tsp, TREND), deparse.level = 0)
-        
-      } else {
-        warning( paste("Window", tsp, "was removed because windowsize < delay*embed", sep = " ") )
-        
-      }
+    } else {
+      # the data come from an already-constructed RP
+      ts1win = ts1[ixs,]
+      ts2win = NA
     }
+    
+    ## check whether the windowsize can accommodate the delay*embed OR the data come from a plot
+    if ((length(ts1win) > delay*embed) || recpt == TRUE){
+      
+      ans = crqa(ts1win, ts1win, delay, embed, rescale,
+                 radius, normalize, mindiagline, minvertline,
+                 tw, whiteline, recpt, side, method, metric,
+                 datatype)
+      
+      RP = ans$RP
+      
+      # if we have an empty recurrence plot, do this
+      if (length(RP) == 1) { RP = vector() }
+      
+      ## if trend needs to be calculated do it here
+      ans = as.numeric( unlist(ans[1:10]) )
+      
+      if (trend == TRUE){
+        
+        if (length(RP) > 0){
+          RP = as.matrix(RP) ## diags() has changed behaviour 
+          NX = ncol(RP)
+          T = vector("numeric", length = NX-1)
+          
+          ## below some test matrices
+          # RP = matrix(1:16,nrow=4)
+          # RP = cbind(c(2,7), c(1,9), c(10, 12))
+          
+          # do we need to span the full matrix or just half?
+          ## create a vector of indeces for all diagonals of the matrix  
+          # ixi_diag = -(ncol(RP)-1):(nrow(RP)-1) 
+          
+          # k = 2
+          for (k in 1:(NX-1)){  ##ixi_diag
+            # print(diags_extract(RP, k)) ## check the diagonal
+            ixi_rec = which(diags_extract(RP, k) != F) ## find recurrence
+            T[k] = length(ixi_rec)/ (NX-k)*100;
+          }
+          
+          Ntau = NX - 1 - round(0.1*NX);
+          
+          ## last 10% of the RP will be skipped
+          p = polyfit(2:(Ntau+1),T[1:Ntau], 1) # slope
+          TREND = 1000 * p[1]
+          ## Webber's definition includes factor 1000
+          
+        } else { TREND = NA}
+      } else {
+        TREND = NA
+      }
+      
+      crqwin = rbind(crqwin, c(ans, tsp, TREND), deparse.level = 0)
+      
+    } else {
+      warning( paste("Window", tsp, "was removed because windowsize < delay*embed", sep = " ") )
+      
+    }
+  }
   
   ## name the measures
   colnames(crqwin) = c("RR", "DET", "NRLINE", "maxL", "L", 
@@ -202,4 +237,3 @@ wincrqa <- function(ts1,
   return(as.data.frame(crqwin))
   
 }
-
